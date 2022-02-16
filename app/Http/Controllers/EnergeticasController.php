f6ab82174e88052c4d28d1c3f7b0309c0507f42a
arnaubalart <arnaubalart@yahoo.com>
2022-02-16 18:25:58 +0100

    first commit

---
+++ app/Http/Controllers/EnergeticasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Energeticas;
+use Illuminate\Http\Request;
+
+class EnergeticasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Energeticas  $energeticas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Energeticas $energeticas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Energeticas  $energeticas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Energeticas $energeticas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Energeticas  $energeticas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Energeticas $energeticas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Energeticas  $energeticas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Energeticas $energeticas)
+    {
+        //
+    }
+}
---
+++ app/Models/Energeticas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Energeticas extends Model
+{
+    use HasFactory;
+}
